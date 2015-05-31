f9dad07dd840815058b6dbc74bad3ce69fcbe968
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-05-31 12:13:51 +0200

    Fixed permissions

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER "Florian Dejonckheere <florian@floriandejonckheere.be>"
 ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update
-RUN apt-get -y install postfix opendkim opendkim-tools sasl2-bin spamassassin spamc
+RUN apt-get -y install syslog-ng postfix opendkim opendkim-tools sasl2-bin spamassassin spamc
 
 # Create app structure
 RUN mkdir -p /app/ /var/mail/ /etc/postfix/
@@ -15,15 +15,15 @@ RUN chmod a+x /app/start.sh
 
 # Add vmail user
 RUN groupadd -g 5000 vmail && useradd vmail -u 5000 -g vmail -s /sbin/nologin -d /var/mail/
-RUN chmod 0777 /var/mail
-RUN chown -R vmail:vmail /var/mail
+RUN chmod 0777 /var/mail/
+RUN chown -R vmail:vmail /var/mail/
 
 # SpamAssassin
-RUN groupadd spamd && useradd -g spamd -s /sbin/nologin -d /var/log/spamassassin spamd
-RUN mkdir -p /var/log/spamassassin
-RUN chown spamd:spamd /var/log/spamassassin
+RUN groupadd spamd && useradd -g spamd -s /sbin/nologin -d /var/log/spamassassin/ spamd
+RUN mkdir -p /var/log/spamassassin/
+RUN chown spamd:spamd /var/log/spamassassin/
 
-VOLUME /var/mail
+VOLUME /var/mail/
 
 EXPOSE 25
 
--- start.sh
+++ start.sh
@@ -7,25 +7,29 @@ function handler() {
 
 trap handler ERR
 
-echo -e "\e[33mRegenerating virtual mappings...\e[33m"
+echo -e "\e[33mRegenerating virtual mappings...\e[0m"
 newaliases
 postmap /etc/postfix/maps/virtual_domains
 postmap /etc/postfix/maps/virtual_users
 postmap /etc/postfix/maps/virtual_alias_domains
 postmap /etc/postfix/maps/virtual_alias_users
 
-chown -R postfix:postfix /etc/postfix/
+echo -e "\e[33mSetting correct permissions...\e[0m"
+touch /var/log/mail.{log,err,warn} /var/log/spamassassin/spamd.log
+chown syslog:adm /var/log/mail.*
+chown -R spamd:spamd /var/log/spamassassin
+postfix set-permissions
 
-echo -e "\e[33mStarting OpenDKIM...\e[33m"
+echo -e "\e[33mStarting syslog...\e[0m"
+service syslog-ng start
+echo -e "\e[33mStarting OpenDKIM...\e[0m"
 service opendkim start
-echo -e "\e[33mStarting saslauthd...\e[33m"
+echo -e "\e[33mStarting saslauthd...\e[0m"
 #~ service saslauthd start
-echo -e "\e[33mStarting SpamAssassin...\e[33m"
+echo -e "\e[33mStarting SpamAssassin...\e[0m"
 service spamassassin start
-echo -e "\e[33mStarting Postfix...\e[33m"
+echo -e "\e[33mStarting Postfix...\e[0m"
 service postfix start
 
-touch /var/log/mail.log /var/log/mail.err /var/log/mail.warn /var/log/spamassassin/spamd.log
-
-echo -e "\e[33mStartup finished\e[33m"
+echo -e "\e[33mStartup finished\e[0m"
 tail -F /var/log/mail.* /var/log/spamassassin/spamd.log
